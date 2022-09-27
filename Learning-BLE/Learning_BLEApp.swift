0ff86c9ab851b5297a2bc5269f428c7e61d63b0a
andresev <andresevaldez@gmail.com>
2022-09-27 16:25:34 -0500

    Initial Commit

---
+++ Learning-BLE/Learning_BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_BLEApp.swift
+//  Learning-BLE
+//
+//  Created by Andres Valdez on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
