8c5c42eb4d2ad15548510b18b7bd52d31b8dc7ba
andresev <andresevaldez@gmail.com>
2022-10-05 09:54:30 -0500

    Discovers characteristics

---
+++ Learning-BLE/BluetoothPeripheralView.swift
@@ -0,0 +1,20 @@
+//
+//  BluetoothPeripheralView.swift
+//  Learning-BLE
+//
+//  Created by Andres Valdez on 10/4/22.
+//
+
+import SwiftUI
+
+struct BluetoothPeripheralView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BluetoothPeripheralView_Previews: PreviewProvider {
+    static var previews: some View {
+        BluetoothPeripheralView()
+    }
+}
--- Learning-BLE/ContentView.swift
+++ Learning-BLE/ContentView.swift
@@ -31,7 +31,7 @@ class BluetoothViewModel: NSObject, ObservableObject {
     }
 }
 
-extension BluetoothViewModel: CBCentralManagerDelegate {
+extension BluetoothViewModel: CBCentralManagerDelegate, CBPeripheralDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == .poweredOn {
             self.centralManager?.scanForPeripherals(withServices: [CBUUID(string: "0x00FF")])
@@ -48,12 +48,54 @@ extension BluetoothViewModel: CBCentralManagerDelegate {
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("Connected!")
+        peripheral.delegate = self
     }
     
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         print("Disconnected!")
     }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print(peripheral.services ?? "No Services")
+        if let services = peripheral.services {
+            for service in services {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverIncludedServicesFor service: CBService, error: Error?) {
+        print(service.characteristics ?? "characteristics are nil")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+
+          for characteristic in characteristics {
+            print(characteristic)
+              if characteristic.properties.contains(.read) {
+                print("\(characteristic.uuid): properties contains .read")
+                peripheral.readValue(for: characteristic)
+              }
+              if characteristic.properties.contains(.notify) {
+                print("\(characteristic.uuid): properties contains .notify")
+              }
+          }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic,
+                    error: Error?) {
+      switch characteristic.uuid {
+      case CBUUID(string: "0x00FF"):
+          print(characteristic.value ?? "no value")
+        default:
+          print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+      }
+    }
+
+    
 }
+
 
 
 struct ContentView: View {
@@ -62,13 +104,21 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                ForEach(bluetoothViewModel.peripherals, id: \.self) { peripheral in
+                ForEach(bluetoothViewModel.peripherals, id: \.self) {peripheral in
                     
                     HStack {
                         Button {
                             bluetoothViewModel.connectToPeripheral(UUID: peripheral.identifier.uuidString)
                         } label: {
                             Text(peripheral.name ?? "unnamed device")
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            peripheral.discoverServices([CBUUID(string: "59462f12-9543-9999-12c8-58b459a2712d".uppercased())]) // put original: nil
+                        } label: {
+                            Text("Services")
                         }
                         
                         Spacer()
